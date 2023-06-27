dd5e2f3899ec5d91748abda9d245f8262f931d59
jcwaldron <125517332+jcwaldron@users.noreply.github.com>
2023-06-27 12:26:45 -0500

    next commit

--- src/articles/articles.controller.js
+++ src/articles/articles.controller.js
@@ -2,23 +2,34 @@ const service = require("./articles.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 function hasData(req, res, next) {
-  if (req.body.data) {
-    return next();
-  }
-  next({ status: 400, message: "body must have data property" });
-}
+    const methodName = "hasData";
+    req.log.debug({ __filename, methodName, body: req.body });
+     if (req.body.data) {
+      req.log.trace({ __filename, methodName, valid: true });
+       return next();
+     }
+    const message = "body must have data property";
+    next({ status: 400, message: message });
+    req.log.trace({ __filename, methodName, valid: false }, message);
+   }
 
-function dataHas(propertyName) {
-  return (request, response, next) => {
-    const { data = {} } = request.body;
-    const value = data[propertyName];
-    if (value) {
-      return next();
-    }
-    next({ status: 400, message: `Article must include a ${propertyName}` });
-  };
-}
+   function dataHas(propertyName) {
 
+      const methodName = `dataHas('${propertyName}')`;
+      return (req, res, next) => {
+        req.log.debug({ __filename, methodName, body: req.body });
+        const { data = {} } = req.body;
+         const value = data[propertyName];
+         if (value) {
+          req.log.trace({ __filename, methodName, valid: true });
+           return next();
+         }
+    
+        const message = `Article must include a ${propertyName}`;
+        next({ status: 400, message: message });
+        req.log.trace({ __filename, methodName, valid: false }, message);
+       };
+     }
 const hasTitle = dataHas("title");
 const hasUrl = dataHas("url");
 const hasSummary = dataHas("summary");
@@ -31,10 +42,13 @@ async function create(req, res) {
 }
 
 async function list(req, res) {
+  const methodName = "list";
+  req.log.debug({__filename, methodName})
   const data = await service.list();
   res.json({
     data,
   });
+  req.log.trace({ __filename, methodName, return: true, data })
 }
 
 module.exports = {
